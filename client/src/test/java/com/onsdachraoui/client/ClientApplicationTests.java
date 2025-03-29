339e3201b720833a3d84557d7b7adc39e764cff5
onsdachraoui <ons.dachraoui@esprit.tn>
2025-03-29 13:40:44 +0200

    first

--- client/src/test/java/com/onsdachraoui/client/ClientApplicationTests.java
+++ client/src/test/java/com/onsdachraoui/client/ClientApplicationTests.java
@@ -1,13 +1,13 @@
-package com.onsdachraoui.student;
+package com.onsdachraoui.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StudentApplicationTests {
+class ClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
